a2a5465fbeed28ba8d66bf189fbf9566e77856aa
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-02-22 11:27:27 -0500

    import style.css to Home component index.jsx, create divs in Home component

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,5 +1,26 @@
+import './style.css';
+
 const Home = () => {
-  return <div>Home component</div>;
+  return (
+    <div className="home">
+      <div className="title">
+        <h1>
+          <p>Hi,</p>
+          <p>My name is Michael</p>
+          <p>I am a Software Engineer</p>
+        </h1>
+        <Link to="about">
+          <button>More Info</button>
+        </Link>
+      </div>
+      <div className="person">
+        <img
+        src={`${process.env.PUBLIC_URL}/no-logo.png`}
+        alt="person picture"
+        /> 
+      </div>
+    </div> 
+  );
 };
 
 export default Home
