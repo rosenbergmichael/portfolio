3e219c4f5e1dcb212969dcc3f72c0c730fed86ff
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-02-23 15:30:35 -0500

    import LinkedIn, GitHub, Medium logos and create links to my pages in Footer component index.jsx

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,37 @@
+const Footer = () => {
+  return <footer>
+      <a 
+        href="https://www.linkedin.com/in/michael-j-rosenberg/" 
+        target="_blank"
+      >
+      <img 
+        src={`${process.env.PUBLIC_URL}/linkedin-logo.png`}
+        alt="LinkedIn-icon"
+      />
+      </a>
+
+      <a 
+        href="https://github.com/rosenbergmichael" 
+        target="_blank"
+      >
+      <img 
+        src={`${process.env.PUBLIC_URL}/GitHub-logo.png`}
+        alt="GitHub-icon"
+      />
+      </a>
+
+      <a 
+        href="https://michaeljrosenberg.medium.com/" 
+        target="_blank"
+      >
+      <img 
+        src={`${process.env.PUBLIC_URL}/Medium-logo.png`}
+        alt="Medium-icon"
+      />
+      </a>
+
+  </footer>
+  
+}
+
+export default Footer
