6e0f11c0c37d661cb1e12623435ba7ac3d0a44e3
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-02-24 16:18:13 -0500

    build About component structure and import/add route in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from './components/Home'
+import About from './components/About'
 import Footer from './components/Footer'
 import './App.css';
 
@@ -9,6 +10,7 @@ function App() {
       <div className="app">
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
         </Switch>
           <Footer />
       </div>
