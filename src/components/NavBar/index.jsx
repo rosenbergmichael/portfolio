66bc1c91bb376da0e69e5ab16355c592cee7b3c4
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-02-25 15:33:23 -0500

    start building out NavBar component structure in index.jsx

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavBar = () => {
+  return <nav className="navbar">
+    <div className="nav-wrapper">
+      <div className="logo">
+
+      </div>
+    </div>
+  </nav>
+}
+
+export default NavBar
