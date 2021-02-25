acbe81a014f1ec6c9681f23b09912e590493bbf9
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-02-25 15:54:05 -0500

    add logo file for menu bars, import Link and create links to pages in Navbar index.jsx

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,12 +1,49 @@
-import React from 'react'
+import { Link } from  'react-router-dom'; 
 
 const NavBar = () => {
   return <nav className="navbar">
     <div className="nav-wrapper">
       <div className="logo">
-
+        <Link to="/">
+          <img 
+            src=""
+            alt="brand"
+          />
+        </Link>
       </div>
     </div>
+    <div className="list-wrapper">
+      <img
+        src={`${process.env.PUBLIC_URL}/menu-bars-logo.png`}
+        alt="Menu bars"
+      />
+      {/* <img
+        src={`${process.env.PUBLIC_URL}/mr-logo.png`}
+        alt="Menu cross"
+      /> */}
+
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>  
+        <li>
+          <Link to="/about">About</Link>
+        </li> 
+        <li>
+          <Link to="/skills">Skills</Link>
+        </li> 
+        <li>
+          <Link to="/portfolio">Portfolio</Link>
+        </li> 
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li> 
+
+      </ul>  
+
+
+
+    </div>  
   </nav>
 }
 
