f21603f39c3356880020949f90ad716fef0f282f
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-02-26 12:37:40 -0500

    build Skills component listing languages, libraries and other

---
+++ src/components/Skills/index.jsx
@@ -0,0 +1,33 @@
+import './style.css';
+
+const Skills = () => {
+  return (
+    <div className="skills">
+      <h1>Skills</h1>
+        <div className="content">
+          <div>
+            <strong>Languages:</strong>
+            <li>Ruby on Rails</li>
+            <li>JavaScript</li>
+            <li>HTML</li>
+            <li>CSS</li>
+          </div>
+          <div>
+            <strong>Libraries:</strong>
+            <li>React</li>
+            <li>Redux</li>
+          </div>
+          <div>
+            <strong>Other:</strong>
+            <li>Node.js</li>
+            <li>SQL</li>
+            <li>JQuery</li>
+            <li>Github</li>
+            <li>VS Code</li>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Skills 
