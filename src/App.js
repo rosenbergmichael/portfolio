75b9536e79d4bf17595fff2d74c0d35b667f48b5
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-03-01 17:12:50 -0500

    import Portfolio component and create route in App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from './components/Home'
 import About from './components/About'
 import Skills from './components/Skills'
+import Portfolio from './components/Portfolio'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
 import './App.css';
@@ -15,6 +16,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
           <Route exact path="/skills" component={Skills} />
+          <Route exact path="/portfolio" component={Portfolio} />
         </Switch>
           <Footer />
       </div>
