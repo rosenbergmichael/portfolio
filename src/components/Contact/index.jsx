c7a9a09a3fa1be8637673e98f29503e191e9f128
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-03-02 10:11:16 -0500

    build out Contact component with phone, email and google map location

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -2,8 +2,22 @@ import './style.css'
 
 const Contact = () => {
   return (
-    <div>
-      
+    <div className="contact">
+      <h1>Say hi!</h1>
+      <iframe 
+      src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=40.748700,%20-73.937880+(Home)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" 
+      frameborder="0"
+      allowFullScreen=""
+      aria-hidden="false"
+      tabIndex="0"
+      ></iframe>
+
+      <h4>Phone</h4>
+      <p>862-219-0320</p>
+
+      <h4>Email</h4>
+      <p>rosenbergmj1@gmail.com</p>
+
     </div>
   )
 }
