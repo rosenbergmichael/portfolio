727ab2d17f7e00f4c855e6275b03c201e326fdf7
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-03-05 09:16:07 -0500

    move programming language logos to below Skills h1, add styling in Skills component style.css

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -4,6 +4,16 @@ const Skills = () => {
   return (
     <div className="skills">
       <h1>Skills</h1>
+      <div className="logos">
+        <img src={`${process.env.PUBLIC_URL}/rubylogo.png`} alt="Ruby-icon" />
+        <img src={`${process.env.PUBLIC_URL}/railslogo.png`} alt="Rails-icon" />
+        <img src={`${process.env.PUBLIC_URL}/jslogo.png`} alt="JavaScript-icon" />
+        <img src={`${process.env.PUBLIC_URL}/reactlogo.png`} alt="React-icon" />
+        <img src={`${process.env.PUBLIC_URL}/reduxlogo.png`} alt="Redux-icon" />
+        <img src={`${process.env.PUBLIC_URL}/htmllogo.png`} alt="HTML-icon" />
+        <img src={`${process.env.PUBLIC_URL}/csslogo.png`} alt="CSS-icon" />
+        </div>
+        <br/>
         <div className="content">
           <div>
             <strong>Languages:</strong>
@@ -26,7 +36,7 @@ const Skills = () => {
             <li>VS Code</li>
           </div>
         </div>
-        <div className="logos">
+        {/* <div className="logos">
         <img src={`${process.env.PUBLIC_URL}/rubylogo.png`} alt="Ruby-icon" />
         <img src={`${process.env.PUBLIC_URL}/railslogo.png`} alt="Rails-icon" />
         <img src={`${process.env.PUBLIC_URL}/jslogo.png`} alt="JavaScript-icon" />
@@ -34,7 +44,7 @@ const Skills = () => {
         <img src={`${process.env.PUBLIC_URL}/reduxlogo.png`} alt="Redux-icon" />
         <img src={`${process.env.PUBLIC_URL}/htmllogo.png`} alt="HTML-icon" />
         <img src={`${process.env.PUBLIC_URL}/csslogo.png`} alt="CSS-icon" />
-        </div>
+        </div> */}
     </div>
   )
 }
